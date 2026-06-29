c4a917daf1b0ef692e63e96e97b7d3fd864f3459
Manan Kandpal <144691681+devxmanan@users.noreply.github.com>
2026-06-29 18:26:33 +0000

    Add solution for "Number of Strings That Appear as Substrings in Word" problem

---
+++ Strings/1967. Number of Strings That Appear as Substrings in Word.ts
@@ -0,0 +1,9 @@
+function numOfStrings(patterns: string[], word: string): number {
+    let count : number = 0;
+    for(const pattern of patterns){
+        if(word.includes(pattern)){
+            count++;
+        }
+    }
+    return count;
+};
